6f855bef2d97a837e60e5c9a03820ef8fd0f9719
nickross327 <32927270+nickross327@users.noreply.github.com>
2025-09-29 13:22:49 +0100

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,13 +2,8 @@ import React from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 
-import base44 from './api/base44Client'
-import { Base44Provider } from '@base44/sdk'
-
 createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Base44Provider client={base44}>
-      <App />
-    </Base44Provider>
+    <App />
   </React.StrictMode>
 )
